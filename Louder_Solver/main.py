e3eb9187a362ee5280fada4ded48253bc87f7c70
jojowei-cooler <lin.xiao.wei60105@gmail.com>
2021-04-05 20:15:31 +0800

    crawler finish 1

---
+++ Louder_Solver/main.py
@@ -0,0 +1,46 @@
+import pygame
+import time
+import requests
+from selenium import webdriver
+# from selenium.webdriver.common.by import By
+
+# the configuration
+ip_url = "http://192.168.43.1:8080/"
+driver = webdriver.Chrome()
+driver.get(ip_url)
+
+# the function that used to warn
+def yell():
+    pygame.init()
+
+    music=pygame.mixer.Sound("louder.mp3") 
+    music.play()
+
+    timeout = time.time() + 2.5
+    while True:
+        test = 0
+        if time.time() > timeout:
+            break
+        
+    pygame.quit()
+
+# get the data from the website
+def get_data():
+    
+    views = driver.find_element_by_xpath('//*[@id="element1"]/span[2]/span[1]')
+    number = (float(views.text))
+    print(number)
+
+    return bool(number > 90)
+
+# main function
+def main():
+    while True:
+        # get the data from the website
+        is_loud = get_data()
+
+        if is_loud==True:
+            yell()
+
+if __name__ == '__main__':
+    main()
--- Louder_Solver/test.py
+++ Louder_Solver/test.py
@@ -6,6 +6,9 @@ ip_url = "http://192.168.43.1:8080/"
 driver = webdriver.Chrome()
 driver.get(ip_url)
 while True:
-    views = driver.find_elements_by_xpath("/html/body/div[3]/div[3]/div/span[2]/span[1]")
-    print(views)
+    views = driver.find_element_by_xpath('//*[@id="element1"]/span[2]/span[1]')
+    
+    number = (float(views.text))
+    print(number)
     time.sleep(5)
+    
